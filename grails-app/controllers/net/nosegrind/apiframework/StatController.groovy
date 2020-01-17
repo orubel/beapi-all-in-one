0f3d2ff5cca4416fe263a9690fc3b132642167d6
Owen Rubel <orubel@gmail.com>
2020-01-17 08:09:39 -0800

    adding more stats reporting

--- grails-app/controllers/net/nosegrind/apiframework/StatController.groovy
+++ grails-app/controllers/net/nosegrind/apiframework/StatController.groovy
@@ -14,6 +14,16 @@ class StatController {
 		Integer time = System.currentTimeMillis()
 		Integer day = (Integer) time/((1000*60*60*24)+1)
 		switch(params.type){
+			case '4':
+				// data for entire month
+				def month = getAllDailyStats()
+				return [stat:month]
+				break
+			case '3':
+				// data for all 24 hr periods
+				def today = getAllHourlyStats()
+				return [stat:today]
+				break
 			case '2':
 				// 12 months
 				def year = getStatsByYear()
@@ -26,7 +36,7 @@ class StatController {
 				break
 			case '0':
 			default:
-				// data for all 24 hr periods
+				// data for 24 hr period
 				def today = getStatsByDay()
 				return [stat:today]
 				break
@@ -99,30 +109,44 @@ class StatController {
 		return stats
 	}
 
-	LinkedHashMap formatDomainObject(Object data){
-		LinkedHashMap newMap = [:]
+	ArrayList getAllHourlyStats(){
+		Calendar cal = Calendar.getInstance();
+		Short dy = cal.get(Calendar.DAY_OF_MONTH);
+		Short mn = cal.get(Calendar.MONTH)
+		Long yr = cal.get(Calendar.YEAR);
 
-		newMap.put('id', data?.id)
-		newMap.put('version', data?.version)
-
-		//DefaultGrailsDomainClass d = new DefaultGrailsDomainClass(data.class)
-
-		DefaultGrailsDomainClass d = grailsApplication?.getArtefact(DomainClassArtefactHandler.TYPE, data.class.getName())
-
-		if (d!=null) {
-			// println("PP:"+d.persistentProperties)
-
-				d?.persistentProperties?.each() { it2 ->
-					if (it2?.name) {
-						if (DomainClassArtefactHandler.isDomainClass(data[it2.name].getClass())) {
-							newMap["${it2.name}Id"] = data[it2.name].id
-						} else {
-							newMap[it2.name] = data[it2.name]
-						}
-					}
-				}
-
+		ArrayList stats = []
+		// init hours
+		for(int i=0;i<=23;i++){
+			stats[i] = ['time':"${i+1}",'count':'0','uri':"null",'username':"null"]
 		}
-		return newMap
+
+		// get data
+		ArrayList data = Stat.executeQuery("select S.hour,count(S.uri),S.uri,P.username from Stat S join S.user P where S.year=? group by S.hour order by S.hour ASC",[yr])
+		data.each(){it ->
+			stats[it[0]-1] = ['time':"${it[0]}",'count':"${it[1]}",'uri':"${it[2]}",'username':"${it[3]}"]
+		}
+
+		return stats
+	}
+
+	ArrayList getAllDailyStats(){
+		ArrayList stats = []
+		Calendar cal = Calendar.getInstance();
+		Short ldom = cal.getActualMaximum(Calendar.DATE)
+		Short mn = cal.get(Calendar.MONTH)
+		Long yr = cal.get(Calendar.YEAR);
+
+		// init hours
+		for(int i=0;i<=(ldom-1);i++){
+			stats[i] = ['time':"${i+1}",'count':'0','uri':"null",'username':"null"]
+		}
+
+		// get data
+		ArrayList data = Stat.executeQuery("select S.day,count(S.uri),S.uri,P.username from Stat S join S.user P where S.year=? group by S.day order by S.day ASC",[yr])
+		data.each(){it ->
+			stats[it[0]-1] = ['time':"${it[0]}",'count':"${it[1]}",'uri':"${it[2]}",'username':"${it[3]}"]
+		}
+		return stats
 	}
 }

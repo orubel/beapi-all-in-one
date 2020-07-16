d6cb9ed92ed5905a7f1a2018108a0dd56993510b
Owen Rubel <orubel@gmail.com>
2020-07-16 16:21:00 -0700

    saving application file

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -1,17 +1,9 @@
 grails.config.locations = [
-<<<<<<< HEAD
     "file:${userHome}/.beapi/beapi_server.yml",
 	"file:${userHome}/.beapi/beapi.groovy",
 	"file:${userHome}/.beapi/beapi.yml",
 	"file:${userHome}/.beapi/beapi_db.yml",
 	"file:${userHome}/.beapi/beapi_api.yml",
 	"file:${userHome}/.beapi/beapi_oauth.yml"
-=======
-    "file:${userHome}/.lawhq/beapi_server.yml",
-	"file:${userHome}/.lawhq/beapi.groovy",
-	"file:${userHome}/.lawhq/beapi.yml",
-	"file:${userHome}/.lawhq/beapi_db.yml",
-	"file:${userHome}/.lawhq/beapi_api.yml"
->>>>>>> d3db2d079e1de09edabe2b82b82abb53f4b81904
 ]
 
